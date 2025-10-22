6aca046d3a788684d34acef0a4c35edd9ba6e8d8
Jan VL <me@janks.dev>
2025-10-22 07:22:10 +0200

    feat(examples): add battery simulation publisher and subscriber

---
+++ examples/ot_publisher.rs
@@ -0,0 +1,335 @@
+use sparkplug_rs::{Message, PayloadBuilder, Publisher, PublisherConfig, Result, Subscriber, SubscriberConfig};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn timestamp() -> String {
+    let now = chrono::Local::now();
+    now.format("%H:%M:%S%.3f").to_string()
+}
+
+struct BatteryState {
+    soc: f64,
+    power: f64,
+    power_setpoint: Option<f64>,
+    control_enabled: bool,
+    nominal_capacity_kwh: f64,
+    nominal_power_kw: f64,
+    pv_power: f64,
+    pv_nominal_kw: f64,
+}
+
+impl BatteryState {
+    fn new(nominal_capacity_kwh: f64, nominal_power_kw: f64, pv_nominal_kw: f64) -> Self {
+        Self {
+            soc: 50.0,
+            power: 0.0,
+            power_setpoint: None,
+            control_enabled: false,
+            nominal_capacity_kwh,
+            nominal_power_kw,
+            pv_power: 0.0,
+            pv_nominal_kw,
+        }
+    }
+
+    fn update(&mut self, dt_secs: f64) {
+        if self.control_enabled {
+            if let Some(sp) = self.power_setpoint {
+                self.power = sp.clamp(-self.nominal_power_kw, self.nominal_power_kw);
+            }
+        } else {
+            let secs_since_epoch = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let hour = (secs_since_epoch / 3600) % 24;
+            let minute = (secs_since_epoch / 60) % 60;
+            let time_of_day = hour as f64 + minute as f64 / 60.0;
+
+            self.power = match hour {
+                0..=5 => -50.0,
+                6..=9 => -100.0,
+                10..=14 => 80.0,
+                15..=17 => 120.0,
+                18..=20 => 60.0,
+                _ => -30.0,
+            };
+
+            self.pv_power = if (6.0..18.0).contains(&time_of_day) {
+                let t = (time_of_day - 12.0).abs() / 6.0;
+                self.pv_nominal_kw * (1.0 - t * t) * 0.85
+            } else {
+                0.0
+            };
+        }
+
+        let energy_delta_kwh = (self.power * dt_secs) / 3600.0;
+        self.soc = (self.soc - (energy_delta_kwh / self.nominal_capacity_kwh * 100.0))
+            .clamp(0.0, 100.0);
+    }
+
+    fn stored_energy_kwh(&self) -> f64 {
+        self.nominal_capacity_kwh * self.soc / 100.0
+    }
+
+    fn charge_avail_kwh(&self) -> f64 {
+        self.nominal_capacity_kwh * (100.0 - self.soc) / 100.0
+    }
+
+    fn discharge_avail_kwh(&self) -> f64 {
+        self.stored_energy_kwh()
+    }
+
+    fn poc_power(&self) -> f64 {
+        self.power + self.pv_power
+    }
+}
+
+fn publish_birth(
+    publisher: &mut Publisher,
+    _device: &str,
+    state: &BatteryState,
+    group: &str,
+    node: &str,
+) -> Result<()> {
+    let mut nbirth = PayloadBuilder::new()?;
+    nbirth.add_string("Properties/GroupID", group)?;
+    nbirth.add_string("Properties/NodeID", node)?;
+    let nbirth_bytes = nbirth.serialize()?;
+    publisher.publish_birth(&nbirth_bytes)?;
+
+    let mut poc_birth = PayloadBuilder::new()?;
+    poc_birth
+        .add_double_with_alias("DATA/POC_P_ACT", 100, state.poc_power())?
+        .add_double_with_alias("DATA/POC_Q_ACT", 101, 0.0)?
+        .add_double_with_alias("DATA/POC_V_ACT", 102, 230.0)?
+        .add_double_with_alias("DATA/POC_F_ACT", 103, 50.0)?
+        .add_bool_with_alias("DATA/POC_METER_AVAIL", 104, true)?;
+    let poc_bytes = poc_birth.serialize()?;
+    publisher.publish_device_birth("POC", &poc_bytes)?;
+
+    let mut bess_birth = PayloadBuilder::new()?;
+    bess_birth
+        .add_double_with_alias("DATA/BESS_SOC_ACT", 200, state.soc)?
+        .add_double_with_alias("DATA/BESS_SOH_ACT", 201, 95.0)?
+        .add_double_with_alias("DATA/BESS_E_ACT", 202, state.stored_energy_kwh())?
+        .add_double_with_alias("DATA/BESS_E_CAP_AVAIL_ACT", 203, state.nominal_capacity_kwh)?
+        .add_double_with_alias("DATA/BESS_E_DISCHARGE_AVAIL_ACT", 204, state.discharge_avail_kwh())?
+        .add_double_with_alias("DATA/BESS_E_CHARGE_AVAIL_ACT", 205, state.charge_avail_kwh())?
+        .add_bool_with_alias("DATA/BESS_AVAIL", 206, true)?
+        .add_double_with_alias("DATA/BESS_P_ACT", 207, state.power)?
+        .add_double_with_alias("DATA/BESS_Q_ACT", 208, 0.0)?
+        .add_double_with_alias("DATA/BESS_P_NOM_ACT", 209, state.nominal_power_kw)?
+        .add_double_with_alias("DATA/BESS_Q_NOM_ACT", 210, 0.0)?
+        .add_double_with_alias("DATA/BESS_P_LIM_MAX_ACT", 211, state.nominal_power_kw)?
+        .add_double_with_alias("DATA/BESS_P_LIM_MIN_ACT", 212, -state.nominal_power_kw)?;
+    let bess_bytes = bess_birth.serialize()?;
+    publisher.publish_device_birth("BESS", &bess_bytes)?;
+
+    let mut pv_birth = PayloadBuilder::new()?;
+    pv_birth
+        .add_double_with_alias("DATA/PV_P_ACT", 300, state.pv_power)?
+        .add_double_with_alias("DATA/PV_Q_ACT", 301, 0.0)?
+        .add_double_with_alias("DATA/PV_P_NOM_ACT", 302, state.pv_nominal_kw)?
+        .add_double_with_alias("DATA/PV_P_LIM_MAX_ACT", 303, state.pv_nominal_kw)?
+        .add_bool_with_alias("DATA/PV_AVAIL", 304, true)?;
+    let pv_bytes = pv_birth.serialize()?;
+    publisher.publish_device_birth("PV", &pv_bytes)?;
+
+    let mut ctrl_birth = PayloadBuilder::new()?;
+    ctrl_birth
+        .add_bool_with_alias("DATA/BESS_P_CTRL_MODE_EN_ACT", 400, state.control_enabled)?
+        .add_double_with_alias("DATA/BESS_P_CTRL_SP_ACT", 401, state.power_setpoint.unwrap_or(0.0))?;
+    let ctrl_bytes = ctrl_birth.serialize()?;
+    publisher.publish_device_birth("CONTROLLER", &ctrl_bytes)?;
+
+    println!("[{}] [{}/{}] Published birth certificates", timestamp(), group, node);
+    Ok(())
+}
+
+fn publish_data(publisher: &mut Publisher, state: &BatteryState, node: &str, verbose: bool) -> Result<()> {
+    let mut poc_data = PayloadBuilder::new()?;
+    poc_data.add_double_by_alias(100, state.poc_power());
+    let poc_bytes = poc_data.serialize()?;
+    publisher.publish_device_data("POC", &poc_bytes)?;
+
+    let mut bess_data = PayloadBuilder::new()?;
+    bess_data
+        .add_double_by_alias(200, state.soc)
+        .add_double_by_alias(202, state.stored_energy_kwh())
+        .add_double_by_alias(204, state.discharge_avail_kwh())
+        .add_double_by_alias(205, state.charge_avail_kwh())
+        .add_double_by_alias(207, state.power);
+    let bess_bytes = bess_data.serialize()?;
+    publisher.publish_device_data("BESS", &bess_bytes)?;
+
+    let mut pv_data = PayloadBuilder::new()?;
+    pv_data.add_double_by_alias(300, state.pv_power);
+    let pv_bytes = pv_data.serialize()?;
+    publisher.publish_device_data("PV", &pv_bytes)?;
+
+    if verbose {
+        println!("[{}] [{}] Published DDATA (POC/BESS/PV)", timestamp(), node);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("OT Publisher - Community Battery Simulator");
+    println!("===========================================\n");
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || r.store(false, Ordering::SeqCst))
+        .expect("Error setting Ctrl-C handler");
+
+    let bal01_state = Arc::new(Mutex::new(BatteryState::new(500.0, 250.0, 100.0)));
+    let cbhs01_state = Arc::new(Mutex::new(BatteryState::new(1000.0, 500.0, 300.0)));
+
+    let bal01_config = PublisherConfig::new(
+        "tcp://localhost:1883",
+        "ot_bal01",
+        "VPP_R2",
+        "BAL01",
+    );
+    let mut bal01_pub = Publisher::new(bal01_config)?;
+    bal01_pub.connect()?;
+
+    let cbhs01_config = PublisherConfig::new(
+        "tcp://localhost:1883",
+        "ot_cbhs01",
+        "VPP4S_R2",
+        "CBHS01",
+    );
+    let mut cbhs01_pub = Publisher::new(cbhs01_config)?;
+    cbhs01_pub.connect()?;
+
+    let bal01_rebirth = Arc::new(AtomicBool::new(false));
+    let cbhs01_rebirth = Arc::new(AtomicBool::new(false));
+
+    let bal01_state_clone = bal01_state.clone();
+    let cbhs01_state_clone = cbhs01_state.clone();
+    let bal01_rebirth_clone = bal01_rebirth.clone();
+    let cbhs01_rebirth_clone = cbhs01_rebirth.clone();
+
+    let cmd_config = SubscriberConfig::new("tcp://localhost:1883", "ot_cmd_listener", "VPP_R2");
+    let mut cmd_sub = Subscriber::new(cmd_config, Box::new(move |msg: Message| {
+        if let Ok(topic) = msg.parse_topic() {
+            if let Some(msg_type) = topic.message_type() {
+                if msg_type.is_command() {
+                    if let Some(node) = topic.edge_node_id() {
+                        if node == "BAL01" && msg_type.as_str() == "NCMD" {
+                            println!("[{}] [VPP_R2/BAL01] Received rebirth request", timestamp());
+                            bal01_rebirth_clone.store(true, Ordering::SeqCst);
+                        } else if node == "BAL01" && topic.device_id() == Some("CONTROLLER") {
+                            handle_device_command(&msg, &bal01_state_clone, "BAL01");
+                        }
+                    }
+                }
+            }
+        }
+    }))?;
+    cmd_sub.connect()?;
+    cmd_sub.subscribe_all()?;
+
+    let cmd_config2 = SubscriberConfig::new("tcp://localhost:1883", "ot_cmd_listener2", "VPP4S_R2");
+    let mut cmd_sub2 = Subscriber::new(cmd_config2, Box::new(move |msg: Message| {
+        if let Ok(topic) = msg.parse_topic() {
+            if let Some(msg_type) = topic.message_type() {
+                if msg_type.is_command() {
+                    if let Some(node) = topic.edge_node_id() {
+                        if node == "CBHS01" && msg_type.as_str() == "NCMD" {
+                            println!("[{}] [VPP4S_R2/CBHS01] Received rebirth request", timestamp());
+                            cbhs01_rebirth_clone.store(true, Ordering::SeqCst);
+                        } else if node == "CBHS01" && topic.device_id() == Some("CONTROLLER") {
+                            handle_device_command(&msg, &cbhs01_state_clone, "CBHS01");
+                        }
+                    }
+                }
+            }
+        }
+    }))?;
+    cmd_sub2.connect()?;
+    cmd_sub2.subscribe_all()?;
+
+    publish_birth(&mut bal01_pub, "BAL01", &bal01_state.lock().unwrap(), "VPP_R2", "BAL01")?;
+    publish_birth(&mut cbhs01_pub, "CBHS01", &cbhs01_state.lock().unwrap(), "VPP4S_R2", "CBHS01")?;
+
+    println!("\nPublishing telemetry (Ctrl+C to stop)...\n");
+
+    let mut counter = 0;
+    while running.load(Ordering::SeqCst) {
+        thread::sleep(Duration::from_secs(5));
+        counter += 1;
+
+        if bal01_rebirth.swap(false, Ordering::SeqCst) {
+            bal01_pub.rebirth()?;
+            publish_birth(&mut bal01_pub, "BAL01", &bal01_state.lock().unwrap(), "VPP_R2", "BAL01")?;
+        }
+
+        if cbhs01_rebirth.swap(false, Ordering::SeqCst) {
+            cbhs01_pub.rebirth()?;
+            publish_birth(&mut cbhs01_pub, "CBHS01", &cbhs01_state.lock().unwrap(), "VPP4S_R2", "CBHS01")?;
+        }
+
+        {
+            let mut state = bal01_state.lock().unwrap();
+            state.update(5.0);
+            publish_data(&mut bal01_pub, &state, "VPP_R2/BAL01", counter % 6 == 0)?;
+        }
+
+        {
+            let mut state = cbhs01_state.lock().unwrap();
+            state.update(5.0);
+            publish_data(&mut cbhs01_pub, &state, "VPP4S_R2/CBHS01", counter % 6 == 0)?;
+        }
+
+        if counter % 6 == 0 {
+            let bal01 = bal01_state.lock().unwrap();
+            let cbhs01 = cbhs01_state.lock().unwrap();
+            println!(
+                "[{}] Cycle {} | BAL01: SOC={:.1}% P={:.1}kW PV={:.1}kW | CBHS01: SOC={:.1}% P={:.1}kW PV={:.1}kW",
+                timestamp(), counter, bal01.soc, bal01.power, bal01.pv_power, cbhs01.soc, cbhs01.power, cbhs01.pv_power
+            );
+        }
+    }
+
+    println!("\nShutting down...");
+    cmd_sub.disconnect()?;
+    cmd_sub2.disconnect()?;
+    bal01_pub.disconnect()?;
+    cbhs01_pub.disconnect()?;
+    println!("Disconnected");
+
+    Ok(())
+}
+
+fn handle_device_command(msg: &Message, state: &Arc<Mutex<BatteryState>>, node: &str) {
+    if let Ok(payload) = msg.parse_payload() {
+        let mut state = state.lock().unwrap();
+        for metric_result in payload.metrics() {
+            if let Ok(metric) = metric_result {
+                if let Some(name) = &metric.name {
+                    match name.as_str() {
+                        "CMD/BESS_P_CTRL_MODE_EN_CMD" => {
+                            if let sparkplug_rs::MetricValue::Boolean(v) = metric.value {
+                                state.control_enabled = v;
+                                println!("[{}] [{}] Control mode: {}", timestamp(), node, if v { "ENABLED" } else { "DISABLED" });
+                            }
+                        }
+                        "CMD/BESS_P_CTRL_SP" => {
+                            if let sparkplug_rs::MetricValue::Double(v) = metric.value {
+                                state.power_setpoint = Some(v);
+                                println!("[{}] [{}] Power setpoint: {:.1} kW", timestamp(), node, v);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ examples/ot_subscriber.rs
@@ -0,0 +1,259 @@
+use sparkplug_rs::{Message, MetricAlias, PayloadBuilder, Publisher, PublisherConfig, Result, Subscriber, SubscriberConfig};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, SystemTime};
+
+fn timestamp() -> String {
+    let now = chrono::Local::now();
+    now.format("%H:%M:%S%.3f").to_string()
+}
+
+#[derive(Debug, Clone)]
+struct NodeState {
+    last_seen: SystemTime,
+    last_seq: Option<u64>,
+    metrics: HashMap<String, f64>,
+    aliases: HashMap<MetricAlias, String>,
+}
+
+impl NodeState {
+    fn new() -> Self {
+        Self {
+            last_seen: SystemTime::now(),
+            last_seq: None,
+            metrics: HashMap::new(),
+            aliases: HashMap::new(),
+        }
+    }
+}
+
+type NodeMap = Arc<Mutex<HashMap<String, NodeState>>>;
+
+fn main() -> Result<()> {
+    println!("OT Subscriber - Monitoring Tool");
+    println!("================================\n");
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || r.store(false, Ordering::SeqCst))
+        .expect("Error setting Ctrl-C handler");
+
+    let nodes: NodeMap = Arc::new(Mutex::new(HashMap::new()));
+
+    let nodes_clone = nodes.clone();
+    let vpp_r2_config = SubscriberConfig::new("tcp://localhost:1883", "ot_monitor_r2", "VPP_R2");
+    let mut vpp_r2_sub = Subscriber::new(vpp_r2_config, Box::new(move |msg: Message| {
+        handle_message(&msg, &nodes_clone, "VPP_R2");
+    }))?;
+    vpp_r2_sub.connect()?;
+    vpp_r2_sub.subscribe_all()?;
+    println!("[{}] [OK] Subscribed to VPP_R2/#", timestamp());
+
+    let nodes_clone2 = nodes.clone();
+    let vpp4s_r2_config = SubscriberConfig::new("tcp://localhost:1883", "ot_monitor_4s", "VPP4S_R2");
+    let mut vpp4s_r2_sub = Subscriber::new(vpp4s_r2_config, Box::new(move |msg: Message| {
+        handle_message(&msg, &nodes_clone2, "VPP4S_R2");
+    }))?;
+    vpp4s_r2_sub.connect()?;
+    vpp4s_r2_sub.subscribe_all()?;
+    println!("[{}] [OK] Subscribed to VPP4S_R2/#", timestamp());
+
+    let cmd_pub_r2_config = PublisherConfig::new(
+        "tcp://localhost:1883",
+        "ot_monitor_cmd_r2",
+        "VPP_R2",
+        "MONITOR",
+    );
+    let mut cmd_pub_r2 = Publisher::new(cmd_pub_r2_config)?;
+    cmd_pub_r2.connect()?;
+
+    let cmd_pub_4s_config = PublisherConfig::new(
+        "tcp://localhost:1883",
+        "ot_monitor_cmd_4s",
+        "VPP4S_R2",
+        "MONITOR",
+    );
+    let mut cmd_pub_4s = Publisher::new(cmd_pub_4s_config)?;
+    cmd_pub_4s.connect()?;
+
+    println!("\nSending rebirth requests to known nodes...");
+    send_rebirth_request(&mut cmd_pub_r2, "BAL01")?;
+    send_rebirth_request(&mut cmd_pub_4s, "CBHS01")?;
+    println!("Rebirth requests sent\n");
+
+    println!("Monitoring messages (Ctrl+C to stop)");
+    println!("Commands:");
+    println!("  - Type 'r <group> <node>' to request rebirth");
+    println!("  - Type 's' to show status");
+    println!("  - Press Ctrl+C to quit\n");
+
+    let mut counter = 0;
+
+    while running.load(Ordering::SeqCst) {
+        thread::sleep(Duration::from_secs(1));
+        counter += 1;
+
+        if counter % 30 == 0 {
+            print_status(&nodes);
+        }
+
+        check_stale_data(&nodes);
+    }
+
+    println!("\nShutting down...");
+    vpp_r2_sub.disconnect()?;
+    vpp4s_r2_sub.disconnect()?;
+    cmd_pub_r2.disconnect()?;
+    cmd_pub_4s.disconnect()?;
+    println!("Disconnected");
+
+    Ok(())
+}
+
+fn send_rebirth_request(publisher: &mut Publisher, node: &str) -> Result<()> {
+    let mut payload = PayloadBuilder::new()?;
+    payload.add_bool("Node Control/Rebirth", true)?;
+    let payload_bytes = payload.serialize()?;
+
+    publisher.publish_node_command(node, &payload_bytes)?;
+    println!("[{}]   → Sent rebirth request to {}", timestamp(), node);
+    Ok(())
+}
+
+fn handle_message(msg: &Message, nodes: &NodeMap, group: &str) {
+    if let Ok(topic) = msg.parse_topic() {
+        if let Some(msg_type) = topic.message_type() {
+            if let Some(node_id) = topic.edge_node_id() {
+                let key = format!("{}/{}", group, node_id);
+                let mut nodes_map = nodes.lock().unwrap();
+                let node = nodes_map.entry(key.clone()).or_insert_with(NodeState::new);
+                node.last_seen = SystemTime::now();
+
+                if msg_type.is_birth() {
+                    let device = topic.device_id().unwrap_or("NODE");
+                    println!("[{}] [{}] {} - BIRTH", timestamp(), key, device);
+
+                    if let Ok(payload) = msg.parse_payload() {
+                        if let Some(seq) = payload.seq() {
+                            node.last_seq = Some(seq);
+                        }
+
+                        for metric_result in payload.metrics() {
+                            if let Ok(metric) = metric_result {
+                                if let Some(name) = &metric.name {
+                                    if let Some(alias) = metric.alias {
+                                        node.aliases.insert(alias, name.clone());
+                                    }
+                                    if let Some(val) = extract_double(&metric.value) {
+                                        node.metrics.insert(name.clone(), val);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else if msg_type.is_data() {
+                    if let Ok(payload) = msg.parse_payload() {
+                        if let Some(seq) = payload.seq() {
+                            if let Some(last_seq) = node.last_seq {
+                                // Sparkplug B sequence numbers are 0-255 (wraps at 256)
+                                let expected_seq = (last_seq + 1) % 256;
+                                if seq != last_seq && seq != expected_seq {
+                                    println!(
+                                        "[{}] [{}] SEQUENCE GAP: expected {}, got {}",
+                                        timestamp(),
+                                        key,
+                                        expected_seq,
+                                        seq
+                                    );
+                                }
+                            }
+                            node.last_seq = Some(seq);
+                        }
+
+                        for metric_result in payload.metrics() {
+                            if let Ok(metric) = metric_result {
+                                let metric_name = if let Some(name) = &metric.name {
+                                    Some(name.clone())
+                                } else if let Some(alias) = metric.alias {
+                                    node.aliases.get(&alias).cloned()
+                                } else {
+                                    None
+                                };
+
+                                if let Some(name) = metric_name {
+                                    if let Some(val) = extract_double(&metric.value) {
+                                        node.metrics.insert(name, val);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else if msg_type.is_death() {
+                    println!("[{}] [{}] NODE DEATH", timestamp(), key);
+                }
+            }
+        }
+    }
+}
+
+fn extract_double(value: &sparkplug_rs::MetricValue) -> Option<f64> {
+    match value {
+        sparkplug_rs::MetricValue::Double(v) => Some(*v),
+        sparkplug_rs::MetricValue::Float(v) => Some(*v as f64),
+        sparkplug_rs::MetricValue::Int64(v) => Some(*v as f64),
+        sparkplug_rs::MetricValue::Int32(v) => Some(*v as f64),
+        _ => None,
+    }
+}
+
+fn print_status(nodes: &NodeMap) {
+    let nodes_map = nodes.lock().unwrap();
+    if nodes_map.is_empty() {
+        println!("\n[{}] [STATUS] No nodes detected", timestamp());
+        return;
+    }
+
+    println!("\n[{}] === Node Status ===", timestamp());
+    for (key, state) in nodes_map.iter() {
+        let age = SystemTime::now()
+            .duration_since(state.last_seen)
+            .unwrap_or(Duration::from_secs(0));
+
+        print!("  {}: ", key);
+        if age.as_secs() > 60 {
+            print!("STALE ({:.0}s ago) ", age.as_secs());
+        } else {
+            print!("ACTIVE ");
+        }
+
+        if let Some(soc) = state.metrics.get("DATA/BESS_SOC_ACT") {
+            print!("SOC={:.1}% ", soc);
+        }
+        if let Some(power) = state.metrics.get("DATA/BESS_P_ACT") {
+            print!("P={:.1}kW ", power);
+        }
+        if let Some(pv) = state.metrics.get("DATA/PV_P_ACT") {
+            print!("PV={:.1}kW ", pv);
+        }
+        if let Some(seq) = state.last_seq {
+            print!("seq={}", seq);
+        }
+        println!();
+    }
+    println!("  =========================\n");
+}
+
+fn check_stale_data(nodes: &NodeMap) {
+    let nodes_map = nodes.lock().unwrap();
+    for (key, state) in nodes_map.iter() {
+        let age = SystemTime::now()
+            .duration_since(state.last_seen)
+            .unwrap_or(Duration::from_secs(0));
+
+        if age.as_secs() > 120 {
+            println!("[{}] [WARNING] {} data is stale ({:.0}s)", timestamp(), key, age.as_secs());
+        }
+    }
+}
